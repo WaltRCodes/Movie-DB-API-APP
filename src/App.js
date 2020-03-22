9153abdd2021b2bea1b9efd16715c7b81a8190d6
Steven Castro <stevencastrod@gmail.com>
2020-03-21 21:34:28 -0400

    updated
    Co-authored-by: Walter Rada <walter.rada@mail.citytech.cuny.edu>
    Co-authored-by: Justin Torres <justin.torres811@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+//firebase module by Netflix developer guy
+import withFirebaseAuth from "react-with-firebase-auth";
+//firebase from google
+import * as firebase from "firebase/app";
+//firebase auth module
+import "firebase/auth";
+//firebase config
+import firebaseConfig from "./firebaseConfig";
+//initilizae the firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+//auth variable
+const firebaseAppAuth = firebaseApp.auth();
+//provide google,facebook,twitter,github
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider()
+};
+class App extends Component {
+  render() {
+    const { user, signOut, signInWithGoogle } = this.props;
+    return (
+      <div className="App">
+        <header className="App-header">
+          {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+          {user ? (
+            <button onClick={signOut}>Sign out</button>
+          ) : (
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+          )}
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+})(App);
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import react, { Component } from "react"
+
+class Navigation extends Component {
+    render(){
+        return (
+            <div>
+              
+            </div>
+          );
+    }
+} 
